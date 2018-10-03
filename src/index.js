56ec5a0c1be3ec2ad6c0f08effdbf94d0008ca45
palina_kutsilina <palina.kutsilina@gmail.com>
2018-10-03 21:34:02 +0300

    Solve longest problem

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 module.exports = function longestConsecutiveLength(array) {
-  // your solution here
+    let hash = {};
+    for (let i = 0; i < array.length; i++) {
+        hash[array[i]] = true;
+    }
+
+    let maxLength = 0;
+    for (let i = 0; i < array.length; i++) {
+        if (!checkValueInHash(hash, array[i] - 1)) {
+            let begin = array[i];
+            let end = array[i] + 1;
+            while (checkValueInHash(hash, end)) {
+                end++;
+            }
+            if (maxLength < end - begin) {
+                maxLength = end - begin;
+            }
+        }
+    }
+    return maxLength;
+};
+
+function checkValueInHash(hash, value) {
+    return hash[value] === true;
 }
